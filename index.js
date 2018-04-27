45ff9505cb5047477cd444da099fa44c3ac36b76
jiaoqiqi <1176932165@qq.com>
2018-04-27 21:59:09 +0800

    use table
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- index.js
+++ index.js
@@ -1,106 +1,140 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { LocaleProvider, DatePicker, message } from 'antd';
-// The default locale is en-US, but we can change it to other language
-import frFR from 'antd/lib/locale-provider/fr_FR';
 import moment from 'moment';
 import 'moment/locale/fr';
-// import { List } from 'antd';
-// import { List, Avatar } from 'antd';
-import { List, Avatar, Button, Spin } from 'antd';
+import {Table} from 'antd';
 import reqwest from 'reqwest';
 
 moment.locale('fr');
-const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
+
+const columns = [{
+    title: '排行榜',
+    dataIndex: 'rankingList',
+    sorter: true,
+    // render: name => `${name.first} ${name.last}`,
+    width: '10%',
+}, {
+    title: '用户名',
+    dataIndex: 'userName',
+    // filters: [
+    //     {text: 'Male', value: 'male'},
+    //     {text: 'Female', value: 'female'},
+    // ],
+    width: '15%',
+}, {
+    title: '描述',
+    dataIndex: 'userInfo',
+    width: '30%',
+},{
+    title: 'AC数',
+    dataIndex: 'passNum',
+    width: '10%',
+},{
+    title: '通过率',
+    dataIndex: 'passRate',
+    width: '15%',
+}];
+
+
 
 const data = [
     {
-        title: 'Ant Design Title 1',
+        rankingList : 1,
+        userName : "q",
+        userInfo : "snfjdsjfnvlksafiumasdkfdshfmsdkvnask",
+        passNum : 345,
+        passRate : 30
     },
     {
-        title: 'Ant Design Title 2',
+        rankingList : 2,
+        userName : "w",
+        userInfo : "snfjdsjfnvlksafiumasdkfdshfmsdkvnask",
+        passNum : 300,
+        passRate : 25
     },
     {
-        title: 'Ant Design Title 3',
+        rankingList : 3,
+        userName : "e",
+        userInfo : "snfjdsjfnvlksafiumasdkfdshfmsdkvnask",
+        passNum : 298,
+        passRate : 20
     },
     {
-        title: 'Ant Design Title 4',
+        rankingList : 4,
+        userName : "r",
+        userInfo : "snfjdsjfnvlksafiumasdkfdshfmsdkvnask",
+        passNum : 257,
+        passRate : 15
+    },{
+        rankingList : 5,
+        userName : "a",
+        userInfo : "snfjdsjfnvlksafiumasdkfdshfmsdkvnask",
+        passNum : 173,
+        passRate : 10
     },
 ];
 
 
 class App extends React.Component {
     state = {
-        loading: true,
-        loadingMore: false,
-        showLoadingMore: true,
         data: [],
-    }
-    componentDidMount() {
-        this.getData((res) => {
-            this.setState({
-                loading: false,
-                data: res.results,
-            });
-        });
-    }
-    getData = (callback) => {
-        reqwest({
-            url: fakeDataUrl,
-            type: 'json',
-            method: 'get',
-            contentType: 'application/json',
-            success: (res) => {
-                callback(res);
-            },
-        });
-    }
-    onLoadMore = () => {
-        this.setState({
-            loadingMore: true,
-        });
-        this.getData((res) => {
-            const data = this.state.data.concat(res.results);
-            this.setState({
-                data,
-                loadingMore: false,
-            }, () => {
-                // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
-                // In real scene, you can using public method of react-virtualized:
-                // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
-                window.dispatchEvent(new Event('resize'));
-            });
-        });
-    }
+        pagination: {},
+        loading: false,
+    };
+    // handleTableChange = (pagination, filters, sorter) => {
+    //     const pager = {...this.state.pagination};
+    //     pager.current = pagination.current;
+    //     this.setState({
+    //         pagination: pager,
+    //     });
+    //     this.fetch({
+    //         results: pagination.pageSize,
+    //         page: pagination.current,
+    //         sortField: sorter.field,
+    //         sortOrder: sorter.order,
+    //         ...filters,
+    //     });
+    // };
+    // fetch = (params = {}) => {
+    //     console.log('params:', params);
+    //     this.setState({loading: true});
+    //     reqwest({
+    //         url: 'https://randomuser.me/api',
+    //         method: 'get',
+    //         data: {
+    //             results: 10,
+    //             ...params,
+    //         },
+    //         type: 'json',
+    //     }).then((data) => {
+    //         const pagination = {...this.state.pagination};
+    //         // Read total count from server
+    //         // pagination.total = data.totalCount;
+    //         pagination.total = 200;
+    //         this.setState({
+    //             loading: false,
+    //             data: data.results,
+    //             pagination,
+    //         });
+    //     });
+    // }
+
+    // componentDidMount() {
+    //     this.fetch();
+    // }
+
     render() {
-        const { loading, loadingMore, showLoadingMore, data } = this.state;
-        const loadMore = showLoadingMore ? (
-            <div style={{ textAlign: 'center', marginTop: 12, height: 32, lineHeight: '32px' }}>
-                {loadingMore && <Spin />}
-                {!loadingMore && <Button onClick={this.onLoadMore}>loading more</Button>}
-            </div>
-        ) : null;
         return (
-            <List
-                className="demo-loadmore-list"
-                loading={loading}
-                itemLayout="horizontal"
-                loadMore={loadMore}
-                dataSource={data}
-                renderItem={item => (
-                    <List.Item actions={[<a>edit</a>, <a>more</a>]}>
-                        <List.Item.Meta
-                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                            title={<a href="https://ant.design">{item.name.last}</a>}
-                            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                        />
-                        <div>content</div>
-                    </List.Item>
-                )}
+            <Table columns={columns}
+                   data={this.state.data}
+                   // rowKey={record => record.registered}
+                   // dataSource={this.state.data}
+                   // pagination={this.state.pagination}
+                   // loading={this.state.loading}
+                   // onChange={this.handleTableChange}
             />
         );
     }
 }
 
-// ReactDOM.render(<LoadMoreList />, mountNode);
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
