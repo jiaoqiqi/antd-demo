642dc2a19e92773ad79b498838259d089508fef5
jiaoqiqi <1176932165@qq.com>
2018-04-27 15:04:37 +0800

    list with load and operation
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ import frFR from 'antd/lib/locale-provider/fr_FR';
 import moment from 'moment';
 import 'moment/locale/fr';
 // import { List } from 'antd';
-import { List, Avatar } from 'antd';
+// import { List, Avatar } from 'antd';
+import { List, Avatar, Button, Spin } from 'antd';
+import reqwest from 'reqwest';
 
 moment.locale('fr');
+const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
 
 const data = [
     {
@@ -27,30 +30,71 @@ const data = [
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: '',
-        };
+    state = {
+        loading: true,
+        loadingMore: false,
+        showLoadingMore: true,
+        data: [],
     }
-    handleChange(date) {
-        message.info('Selected Date: ' + (date ? date.toString() : ''));
-        this.setState({ date });
+    componentDidMount() {
+        this.getData((res) => {
+            this.setState({
+                loading: false,
+                data: res.results,
+            });
+        });
     }
-
-
+    getData = (callback) => {
+        reqwest({
+            url: fakeDataUrl,
+            type: 'json',
+            method: 'get',
+            contentType: 'application/json',
+            success: (res) => {
+                callback(res);
+            },
+        });
+    }
+    onLoadMore = () => {
+        this.setState({
+            loadingMore: true,
+        });
+        this.getData((res) => {
+            const data = this.state.data.concat(res.results);
+            this.setState({
+                data,
+                loadingMore: false,
+            }, () => {
+                // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
+                // In real scene, you can using public method of react-virtualized:
+                // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
+                window.dispatchEvent(new Event('resize'));
+            });
+        });
+    }
     render() {
+        const { loading, loadingMore, showLoadingMore, data } = this.state;
+        const loadMore = showLoadingMore ? (
+            <div style={{ textAlign: 'center', marginTop: 12, height: 32, lineHeight: '32px' }}>
+                {loadingMore && <Spin />}
+                {!loadingMore && <Button onClick={this.onLoadMore}>loading more</Button>}
+            </div>
+        ) : null;
         return (
             <List
+                className="demo-loadmore-list"
+                loading={loading}
                 itemLayout="horizontal"
+                loadMore={loadMore}
                 dataSource={data}
                 renderItem={item => (
-                    <List.Item>
+                    <List.Item actions={[<a>edit</a>, <a>more</a>]}>
                         <List.Item.Meta
                             avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                            title={<a href="https://ant.design">{item.title}</a>}
+                            title={<a href="https://ant.design">{item.name.last}</a>}
                             description="Ant Design, a design language for background applications, is refined by Ant UED Team"
                         />
+                        <div>content</div>
                     </List.Item>
                 )}
             />
@@ -58,4 +102,5 @@ class App extends React.Component {
     }
 }
 
+// ReactDOM.render(<LoadMoreList />, mountNode);
 ReactDOM.render(<App />, document.getElementById('root'));
