72ee4b7152c14087a0e74a90bdce43b9eec2a8a1
jiaoqiqi <1176932165@qq.com>
2018-05-03 15:19:32 +0800

    导航
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- index.js
+++ index.js
@@ -2,48 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
 import 'moment/locale/fr';
-import { List, Avatar } from 'antd';
-import { Menu, Dropdown, Icon } from 'antd';
-
-
+import { Menu, Dropdown, Icon} from 'antd';
 
 moment.locale('fr');
 
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
-const menu = (
-    <Menu>
-        <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
-        </Menu.Item>
-        <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
-        </Menu.Item>
-        <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
-        </Menu.Item>
-        <Menu.Item disabled>3rd menu item（disabled）</Menu.Item>
-
-    </Menu>
-);
 
 class App extends React.Component {
-
+    handleClick = (e) => {
+        console.log('click ', e);
+    };
     render() {
-
         return (
-            <Dropdown overlay={menu}>
-                <a className="ant-dropdown-link" href="#">
-                    Hover me <Icon type="down" />
-                </a>
-            </Dropdown>
+            <Menu
+                onClick={this.handleClick}
+                style={{ width: 256 }}
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+            >
+                <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                    <MenuItemGroup key="g1" title="Item 1">
+                        <Menu.Item key="1">Option 1</Menu.Item>
+                        <Menu.Item key="2">Option 2</Menu.Item>
+                    </MenuItemGroup>
+                    <MenuItemGroup key="g2" title="Item 2">
+                        <Menu.Item key="3">Option 3</Menu.Item>
+                        <Menu.Item key="4">Option 4</Menu.Item>
+                    </MenuItemGroup>
+                </SubMenu>
+                <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
+                    <Menu.Item key="5">Option 5</Menu.Item>
+                    <Menu.Item key="6">Option 6</Menu.Item>
+                    <SubMenu key="sub3" title="Submenu">
+                        <Menu.Item key="7">Option 7</Menu.Item>
+                        <Menu.Item key="8">Option 8</Menu.Item>
+                    </SubMenu>
+                </SubMenu>
+                <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
+                    <Menu.Item key="9">Option 9</Menu.Item>
+                    <Menu.Item key="10">Option 10</Menu.Item>
+                    <Menu.Item key="11">Option 11</Menu.Item>
+                    <Menu.Item key="12">Option 12</Menu.Item>
+                </SubMenu>
+            </Menu>
         );
     }
 }
 
-// const WrappedRegistrationForm = Form.create()(RegistrationForm);
 ReactDOM.render(<App/>, document.getElementById('root'));
-
-
-// ReactDOM.render(<WrappedRegistrationForm />, mountNode);
-
-// ReactDOM.render(<App/>, document.getElementById('root'));
