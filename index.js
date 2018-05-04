a2b6d16db27c56e0fc7c97b2bae7d860d377d814
jiaoqiqi <1176932165@qq.com>
2018-05-04 22:46:18 +0800

    模态框
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- index.js
+++ index.js
@@ -3,57 +3,93 @@ import ReactDOM from 'react-dom';
 import moment from 'moment';
 import 'moment/locale/fr';
 import "./index.css"
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Button, Modal, Form, Input, Radio } from 'antd';
 moment.locale('fr');
 
 const FormItem = Form.Item;
 
+const CollectionCreateForm = Form.create()(
+    class extends React.Component {
+        render() {
+            const { visible, onCancel, onCreate, form } = this.props;
+            const { getFieldDecorator } = form;
+            return (
+                <Modal
+                    visible={visible}
+                    title="Create a new collection"
+                    okText="Create"
+                    onCancel={onCancel}
+                    onOk={onCreate}
+                >
+                    <Form layout="vertical">
+                        <FormItem label="Title">
+                            {getFieldDecorator('title', {
+                                rules: [{ required: true, message: 'Please input the title of collection!' }],
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                        <FormItem label="Description">
+                            {getFieldDecorator('description')(<Input type="textarea" />)}
+                        </FormItem>
+                        <FormItem className="collection-create-form_last-form-item">
+                            {getFieldDecorator('modifier', {
+                                initialValue: 'public',
+                            })(
+                                <Radio.Group>
+                                    <Radio value="public">Public</Radio>
+                                    <Radio value="private">Private</Radio>
+                                </Radio.Group>
+                            )}
+                        </FormItem>
+                    </Form>
+                </Modal>
+            );
+        }
+    }
+);
+
+
 class App extends React.Component {
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
+    state = {
+        visible: false,
+    };
+    showModal = () => {
+        this.setState({ visible: true });
+    }
+    handleCancel = () => {
+        this.setState({ visible: false });
+    }
+    handleCreate = () => {
+        const form = this.formRef.props.form;
+        form.validateFields((err, values) => {
+            if (err) {
+                return;
             }
+
+            console.log('Received values of form: ', values);
+            form.resetFields();
+            this.setState({ visible: false });
         });
     }
+    saveFormRef = (formRef) => {
+        this.formRef = formRef;
+    }
     render() {
-        const { getFieldDecorator } = this.props.form;
         return (
-            <Form onSubmit={this.handleSubmit} className="login-form">
-                <FormItem>
-                    {getFieldDecorator('userName', {
-                        rules: [{ required: true, message: 'Please input your username!' }],
-                    })(
-                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                    )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('password', {
-                        rules: [{ required: true, message: 'Please input your Password!' }],
-                    })(
-                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                    )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('remember', {
-                        valuePropName: 'checked',
-                        initialValue: true,
-                    })(
-                        <Checkbox>Remember me</Checkbox>
-                    )}
-                    <a className="login-form-forgot" href="">Forgot password</a>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        Log in
-                    </Button>
-                    Or <a href="">register now!</a>
-                </FormItem>
-            </Form>
+            <div>
+                <Button type="primary" onClick={this.showModal}>New Collection</Button>
+                <CollectionCreateForm
+                    wrappedComponentRef={this.saveFormRef}
+                    visible={this.state.visible}
+                    onCancel={this.handleCancel}
+                    onCreate={this.handleCreate}
+                />
+            </div>
         );
     }
+
 }
 
-const Appp = Form.create()(App);
 
-
-ReactDOM.render(<Appp/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
